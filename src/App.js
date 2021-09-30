d868aaccbb5877ecbb32e16fe8e31d349d6b6f50
OtabekAbdurakhmonov <77769473+OtabekAbdurakhmonov@users.noreply.github.com>
2021-09-30 22:55:01 +0900

    changes done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Post from './post';
+import './App.css'
 
-function App() {
+
+const App = ({initialCount}) => {
+
+  const [state, setState] = useState({
+    count: initialCount,
+    user: "ITPOWER"
+  });
+
+  let [posts, setPosts] = useState([
+    {
+      name: 'This is React Hook',
+      body: 'You can learn by doing this'
+    },
+    {
+      name: 'JavaScript is important',
+      body: 'With the help of it you will succssed'
+    },
+  ])
+
+  const restOne = () => {
+    setState({
+      ...state,
+      count:state.count-1
+    })
+  }
+
+  const addOnePost = () => {
+    let newPost = {
+      name: 'CSS is still vital in React',
+      body: 'You still need to design'
+    }
+
+    setPosts([
+      ...posts,
+      newPost
+    ])
+
+  }
+
+  const removePosts = () => {
+    setPosts([])
+  }
+
+
+  useEffect(() => {
+    console.log('change on state')
+  }, [state])
+
+  useEffect(() => {
+    console.log('change on posts')
+  }, [posts])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container"> 
+        <h1>{state.user}</h1>
+        <h3>Count: <span>{state.count}</span></h3>
+        <button onClick={restOne}>Rest one -1</button>
+        <button onClick={ () => setState ({
+          ...state,
+          count:state.count+1
+        })}>Add one +1</button>
+        <button onClick={removePosts}>Remove Posts</button>
+
+        <hr/>
+
+        {posts.map((item, i) =>(
+          <Post item={item} key={i}/>
+        ))}
+
+        <button
+          onClick={addOnePost}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Add one more
+        </button>
+
+      </div>
+    </>
   );
 }
 
