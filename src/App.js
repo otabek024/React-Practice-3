094c2063a2844a9607880d9d6ea3cbc0fe4826dd
OtabekAbdurakhmonov <77769473+OtabekAbdurakhmonov@users.noreply.github.com>
2021-09-30 23:49:54 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,85 +1,29 @@
-import React, {useState, useEffect} from 'react'
-import Post from './post';
-import './App.css'
+import React, {useState} from 'react'
+import Title from './components/title';
+import Count from './components/count';
+import CountBtn from './components/countBtn';
+import Age from './components/age';
+import AgeBtn from './components/ageBtn';
 
 
 const App = ({initialCount}) => {
-
-  const [state, setState] = useState({
-    count: initialCount,
-    user: "ITPOWER"
-  });
-
-  let [posts, setPosts] = useState([
-    {
-      name: 'This is React Hook',
-      body: 'You can learn by doing this'
-    },
-    {
-      name: 'JavaScript is important',
-      body: 'With the help of it you will succssed'
-    },
-  ])
-
-  const restOne = () => {
-    setState({
-      ...state,
-      count:state.count-1
-    })
-  }
-
-  const addOnePost = () => {
-    let newPost = {
-      name: 'CSS is still vital in React',
-      body: 'You still need to design'
-    }
-
-    setPosts([
-      ...posts,
-      newPost
-    ])
-
-  }
-
-  const removePosts = () => {
-    setPosts([])
-  }
-
-
-  useEffect(() => {
-    console.log('change on state')
-  }, [state])
-
-  useEffect(() => {
-    console.log('change on posts')
-  }, [posts])
+  const[count, setCount] = useState(0);
+  const[age, setAge] = useState(10);  
   
-
-  return (
+  const incrementCount = () => {
+    setCount(count+1);
+  }
+  const incrementAge = () => {
+    setAge(age+1);
+  }
+  
+  return(
     <>
-      <div className="container"> 
-        <h1>{state.user}</h1>
-        <h3>Count: <span>{state.count}</span></h3>
-        <button onClick={restOne}>Rest one -1</button>
-        <button onClick={ () => setState ({
-          ...state,
-          count:state.count+1
-        })}>Add one +1</button>
-        <button onClick={removePosts}>Remove Posts</button>
-
-        <hr/>
-
-        {posts.map((item, i) =>(
-          <Post item={item} key={i}/>
-        ))}
-
-        <button
-          onClick={addOnePost}
-        >
-          Add one more
-        </button>
-
-      </div>
+      <Title/>
+      <Count count={count}/>
+      <CountBtn handleCount={incrementCount}/>
+      <Age age={age}/>
+      <AgeBtn handleAge={incrementAge}/>
     </>
   );
 }
